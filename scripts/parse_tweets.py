e95d6b1b0a72f4df59d6def664fe508f439d1754
Chris Tokita <christopher.tokita@gmail.com>
2020-04-13 17:33:15 -0400

    First run of full tweet parsing script

--- scripts/parse_tweets.py
+++ scripts/parse_tweets.py
@@ -30,10 +30,7 @@ def tweet_parser(filename):
     # OUTPUT:
     # - tweet_data: dataframe of all tweet data in the specified file, with a subset of the relevant data headings (pandas dataframe).
     #
-    tweet_data = pd.DataFrame(columns = ['user_id', 'user_name',
-                                         'tweet_time', 'tweet_text', 'tweet_id',
-                                         'urls', 'urls_expanded',
-                                         'is_quote', 'is_retweet', 'tweet_url'])
+    
     with open(filename,) as json_file:
         # Parse each individual tweet in data set 
         for tweet in json_file:
@@ -45,13 +42,25 @@ def tweet_parser(filename):
             user_id = data['user']['id']
             user_name = data['user']['screen_name']
             tweet_time = data['created_at']
-            tweet_text = data['text']
             tweet_id = data['id']
+            favorite_count = data['favorite_count']
+            retweet_count = data['retweet_count']
+            quote_count = data['quote_count']
+            reply_count = data['reply_count']
             quote_status = data['is_quote_status']
+            retweet_status = 'retweeted_status' in data
             tweet_url = "https://twitter.com/" + user_name + "/status/" + str(tweet_id)
             
-            # Parse urls (cane contain multiple and are in nested list)
-            url_collection = data['entities']['urls']
+            # If extended tweet (>140 char) get full data
+            extended_tweet = 'extended_tweet' in data
+            if extended_tweet:
+                extended_data = data['extended_tweet']
+                tweet_text = extended_data['full_text']
+                url_collection = extended_data['entities']['urls']
+            else: 
+                url_collection = data['entities']['urls']
+                tweet_text = data['text']
+            # Parse urls (can contain multiple and are in nested list)
             url_count = len(url_collection)
             if url_count > 0:
                 urls_list = []
@@ -65,14 +74,21 @@ def tweet_parser(filename):
                 urls_list = ""
                 urls_expanded_list = ""
             
-            # Check if rewteeted and if so, get info
-            if 'retweeted_status' in data:
-                retweet_status = True
+            # Check if rewteeted or quoted tweet, and if so, get info
+            if quote_status:
+                retweet_id = data['quoted_status']['id']
+                rt_user = data['quoted_status']['user']
+                retweet_user_id = rt_user['id']
+                retweet_user_name = rt_user['screen_name']
+            elif retweet_status:
+                retweet_id = data['retweeted_status']['id']
                 rt_user = data['retweeted_status']['user']
                 retweet_user_id = rt_user['id']
+                retweet_user_name = rt_user['screen_name']
             else:
-                retweet_status = False
+                retweet_id = np.nan
                 retweet_user_id = np.nan
+                retweet_user_name = np.nan
             
             # Append to tweet dataframe
             this_tweet = pd.DataFrame({'user_id': user_id, 
@@ -85,9 +101,19 @@ def tweet_parser(filename):
                                        'url_count': url_count,
                                        'is_quote': quote_status,
                                        'is_retweet': retweet_status, 
+                                       'is_extended_tweet': extended_tweet,
+                                       'favorite_count': favorite_count,
+                                       'retweet_count': retweet_count,
+                                       'quote_count': quote_count,
+                                       'reply_count': reply_count,
                                        'retweeted_user_id': retweet_user_id,
+                                       'rewteeted_user_name': retweet_user_name,
+                                       'retweet_id': retweet_id,
                                        'tweet_url': tweet_url}, index = [0])
-            tweet_data = tweet_data.append(this_tweet, ignore_index = True, sort = False)
+            try:
+                tweet_data = tweet_data.append(this_tweet, ignore_index = True, sort = False)
+            except:
+                tweet_data = this_tweet
             
     return tweet_data
 
@@ -96,18 +122,25 @@ def tweet_parser(filename):
 # Load data
 ####################
 # Speficy files to be read
-json1 = "../data/tweets/crowdsource_factchecking_prem2.json"
-json2 = "../data/tweets/crowdsource_factchecking.json"
-json3 = "../data/tweets/crowdsource_factchecking_enterprise_trial.json"
-json_files = [json1, json2, json3]
+save_file_name = "parsed_tweets"
+#source_directory = "../" #local files   
+source_directory = "/Volumes/CKT-DATA/" #external hard drive
+json1 = "data/tweets/raw_json/crowdsource_factchecking_prem2.json"
+json2 = "data/tweets/raw_json/crowdsource_factchecking.json"
+json3 = "data/tweets/raw_json/crowdsource_factchecking_enterprise_trial.json"
+json_files = [json1, json2, json3,]
+json_files = [source_directory + file for file in json_files]
 
 # Parse data
 if 'tweet_data' in globals():
     del tweet_data
 for file in json_files:
     data = tweet_parser(file)
-    if 'tweet_data' not in globals():
-        tweet_data = copy.deepcopy(data)
-    else:
+    try:
         tweet_data = tweet_data.append(data, ignore_index = True, sort = False)
+    except:
+        tweet_data = copy.deepcopy(data)
+    del data
 
+out_path = source_directory + "data/tweets/" + save_file_name + ".csv"
+tweet_data.to_csv(out_path, index = False)
