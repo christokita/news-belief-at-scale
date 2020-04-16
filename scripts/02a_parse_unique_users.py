ddd4c4bdb7c9166308665cffff3288b797668389
Chris Tokita <christopher.tokita@gmail.com>
2020-04-16 11:20:47 -0400

    Restructure code

---
+++ scripts/02a_parse_unique_users.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 14 10:44:52 2020
+
+@author: ChrisTokita
+
+SCRIPT:
+This script uses a high-performance computing cluster to
+parse user data to determine the unique number and IDs of users (and user types).
+"""
+
+####################
+# Load libraries and packages, set important parameters
+####################
+import pandas as pd
+import numpy as np
+import os
+import re
+import sys
+
+# Get file numbers
+i = sys.argv[1] # get which chunk of the 
+
+# path to data
+data_directory = "/scratch/gpfs/ctokita/fake-news-diffusion/"
+tweet_file = data_directory + "data_derived/tweets/parsed_tweets.csv"
+path_to_followers = data_directory + "data/followers/"
+
+# Desired outpaths for writing new data files
+outpath_highlevel_data = data_directory + "data_derived/"
+outpath_tweeters = data_directory + "data_derived/tweets/"
+outpath_followers = data_directory + "data_derived/followers/"
+
+
+####################
+# Unique user count
+####################
+# Get unique IDs of tweeters
+tweet_data = pd.read_csv(tweet_file)
+tweeters = np.unique(tweet_data['user_id'])
+
+# Get unique IDs of followers
+follower_files = os.listdir(path_to_followers)
+follower_files = [file for file in follower_files if re.match('[0-9]', file)] #filter out hidden copies of same files
+follower_files = follower_files[ (865*i) : (865*i+1) ] #there are 86,370 files and we want 100 chunks
+
+followers = np.array([])
+for file in follower_files:
+    follower_list = pd.read_csv(path_to_followers + file)
+    followers = np.append(followers, follower_list)
+followers = np.unique(followers)
+followers = followers.astype(int)
+followers_excl_tweeters = np.setdiff1d(followers, tweeters) #remove followers who are in tweeter set
+
+# Measure number of users and write out to file
+tweeters = pd.DataFrame(tweeters, columns = ['tweeter_id'])
+followers = pd.DataFrame(followers, columns = ['follower_id'])
+followers_excl_tweeters = pd.DataFrame(followers_excl_tweeters, columns = ['follower_id'])
+
+chunk_label = str(i).zfill(2)
+tweeters.to_csv(outpath_tweeters + "unique_tweeters_" + chunk_label + "csv", index = False)
+followers.to_csv(outpath_followers + "unique_followers_" + chunk_label + ".csv", index = False)
+followers_excl_tweeters.to_csv(outpath_followers + "unique_followers_excludingtweeters_" + chunk_label + ".csv", index = False)
--- scripts/suppl_code/02_unique_individuals_local.py
+++ scripts/suppl_code/02_unique_individuals_local.py
@@ -35,17 +35,24 @@ tweeters = np.unique(tweet_data['user_id'])
 follower_files = os.listdir(path_to_followers)
 follower_files = [file for file in follower_files if re.match('[0-9]', file)] #filter out hidden copies of same files
 
-all_followers = np.array([])
+followers = np.array([])
 for file in follower_files:
-    followers = pd.read_csv(path_to_followers + file)
-    all_followers = np.append(all_followers, followers)
-all_followers = np.unique(all_followers)
+    follower_list = pd.read_csv(path_to_followers + file)
+    followers = np.append(followers, follower_list)
+followers = np.unique(followers)
+followers = followers.astype(int)
+followers_excl_tweeters = np.setdiff1d(followers, tweeters) #remove followers who are in tweeter set
+
+
 
 # Measure number of users and write out to file
-unique_users = pd.DataFrame({'user_type': ["Tweeters", "Followers", "Total"], 'count': [len(tweeters), len(all_followers), len(tweeters) + len(all_followers)]})
-all_followers = pd.DataFrame(all_followers, columns = ['follwer_id'])
+unique_users = pd.DataFrame({'user_type': ["Tweeters", "Followers", "Followers excluding tweeters", "Total"], 
+                             'count': [len(tweeters), len(followers), len(followers_excl_tweeters), len(tweeters) + len(followers_excl_tweeters)]})
 tweeters = pd.DataFrame(tweeters, columns = ['tweeter_id'])
+followers = pd.DataFrame(followers, columns = ['follower_id'])
+followers_excl_tweeters = pd.DataFrame(followers_excl_tweeters, columns = ['follower_id'])
 
 unique_users.to_csv(outpath_highlevel_data + "unique_user_summary.csv", index = False)
 tweeters.to_csv(outpath_tweeters + "unique_tweeters.csv", index = False)
-all_followers.to_csv(outpath_followers + "unique_followers.csv", index = False)
+followers.to_csv(outpath_followers + "unique_followers.csv", index = False)
+followers_excl_tweeters.to_csv(outpath_followers + "unique_followers_excludingtweeters.csv", index = False)
