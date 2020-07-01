f0964df6a871f4aeb5dcd3a0166a5c365d326711
Chris Tokita <christopher.tokita@gmail.com>
2020-06-30 21:01:08 -0400

    plot percentage of exposure over time

--- scripts/plot_tweettimeseries.R
+++ scripts/plot_tweettimeseries.R
@@ -25,7 +25,7 @@ label_veracity <- c("T" = "Real",
                     "FM" = "Fake")
 
 
-############################## Plot time series of stories ##############################
+############################## Plot time series of article shares ##############################
 
 ####################
 # Load data 
@@ -44,13 +44,12 @@ tweeter_scores <- tweeter_scores %>%
   mutate(article_first_time = min(tweet_time)) %>% 
   mutate(tweet_number = 1:length(tweet_time), #order tweets for plotting purposes
          relative_tweet_time = as.numeric( (tweet_time - article_first_time) / (60*60) ) ) %>%  #time diff is in seconds, so convert to hours
-  mutate(hour_bin = cut(relative_tweet_time, breaks = seq(0, 50, 1), include.lowest = TRUE, labels = seq(0, 49)))
+  mutate(tweet_count_percentile = tweet_number / max(tweet_number),
+         hour_bin = cut(relative_tweet_time, breaks = seq(0, 50, 1), include.lowest = TRUE, labels = seq(0, 49)))
 
-tweet_timecount <- tweeter_scores %>% 
+tweet_perhour <- tweeter_scores %>% 
   group_by(article_fc_rating, total_article_number, hour_bin) %>% 
-  # count(.)
-  summarise(count = sum(z))
-
+  count(.)
 
 
 ####################
@@ -58,8 +57,8 @@ tweet_timecount <- tweeter_scores %>%
 ####################
 pal <- rev(brewer.pal(5, "RdYlBu"))
 
-# tweets over time
-gg_tweettime <- tweet_timecount %>% 
+# New tweets over time
+gg_tweettime <- tweet_perhour %>% 
   filter(article_fc_rating %in% c("T", "FM")) %>% 
   ggplot(., aes(x = hour_bin, y = n, group = total_article_number)) +
   geom_line(size = 0.2, alpha = 0.5, color = "#495867") +
@@ -74,8 +73,7 @@ gg_tweettime <- tweet_timecount %>%
              labeller = labeller(article_fc_rating = label_veracity))
 gg_tweettime
 
-
-gg_tweettime <- tweet_timecount %>% 
+gg_tweettime <- tweet_perhour %>% 
   filter(article_fc_rating %in% c("T", "FM")) %>% 
   mutate(article = as.factor(as.character(total_article_number))) %>% 
   ggplot(., aes(x = hour_bin, y = article, fill = n)) +
@@ -114,3 +112,104 @@ gg_totaltweets <- tweeter_scores %>%
              labeller = labeller(article_fc_rating = label_veracity))
 gg_totaltweets
 ggsave(gg_totaltweets, filename = "output/timeseries/timeseries_totaltweetcount.png", width = 90, height = 45, units = "mm", dpi = 400)
+
+# Percentiage of tweets per story
+gg_perctweets <- tweeter_scores %>% 
+  filter(article_fc_rating %in% c("T", "FM")) %>% 
+  ggplot(., aes(x = relative_tweet_time, y = tweet_count_percentile, group = total_article_number)) +
+  geom_vline(aes(xintercept = 24), 
+             linetype = "dotted",
+             size = 0.3,
+             color = "grey60") +
+  # geom_text(aes(x = 24, y = 100, label = "24 hr intervention\n"), angle = 270, color = "grey80") +
+  geom_line(size = 0.2, alpha = 0.5, color = "#495867") +
+  xlab("Time since first article share (hrs)") +
+  ylab("Proportion of story stweets") +
+  theme_ctokita() +
+  theme(aspect.ratio = NULL) +
+  facet_wrap(~article_fc_rating, 
+             ncol = 1,
+             strip.position = "right",
+             labeller = labeller(article_fc_rating = label_veracity))
+gg_perctweets
+ggsave(gg_perctweets, filename = "output/timeseries/timeseries_percentagestorytweets.png", width = 90, height = 45, units = "mm", dpi = 400)
+
+####################
+# Plot saturation time of stories
+####################
+percentile <- 0.5
+  
+gg_saturationcount <- tweeter_scores %>% 
+  filter(tweet_count_percentile >= percentile,
+         article_fc_rating %in% c("T", "FM")) %>% 
+  group_by(total_article_number) %>% 
+  filter(tweet_count_percentile == min(tweet_count_percentile)) %>% 
+  ggplot(., aes(x = relative_tweet_time)) +
+  geom_histogram(aes(y = stat(density))) +
+  xlab(paste0("Time to ", percentile*100, "% sharing saturation")) +
+  theme_ctokita() +
+  facet_wrap(~article_fc_rating, 
+             ncol = 1,
+             strip.position = "right",
+             labeller = labeller(article_fc_rating = label_veracity))
+gg_saturationcount
+
+
+############################## Plot time series of article exposure ##############################
+
+####################
+# Plot user exposed to stories over time
+####################
+# TEMP: Load data and bind
+files <- list.files('/Volumes/CKT-DATA/fake-news-diffusion/data_derived/timeseries/individual_articles/', full.names = TRUE)
+for (file in files) {
+  story_data <- read.csv(file) 
+  story_data <- story_data %>%
+    # add dummy rows of pre-tweet data for plotting purposes
+    add_row(X = 0, time = c(-1, -0.01), tweet_number = c(-2, -1), user_id = min(story_data$user_id), new_exposed_users = 0, cumulative_exposed = 0, total_article_number = unique(story_data$total_article_number))
+  if (!exists("exposure_data")) {
+    exposure_data <- story_data
+  } else {
+    exposure_data <- rbind(exposure_data, story_data)
+  }
+}
+
+# Merge in relevant article level data
+article_data <- tweeter_scores %>% 
+  select(user_id, total_article_number, source_lean, article_fc_rating, article_lean, user_ideology, article_ideology)
+exposure_timeseries <- merge(exposure_data, article_data, by = c("user_id", "total_article_number"), all.x = TRUE) %>% 
+  group_by(total_article_number) %>% 
+  mutate(relative_cumulative_exposed = cumulative_exposed / max(cumulative_exposed))
+
+# Total cumulative exposed
+gg_exposuretime <- ggplot(exposure_timeseries, aes(x = time, y = cumulative_exposed, group = total_article_number)) +
+  geom_line(size = 0.3, alpha = 0.5) +
+  # scale_y_log10() +
+  scale_y_continuous(labels = scales::comma) +
+  xlab("Time since first article share (hrs)") +
+  ylab("Total users exposed") +
+  theme_ctokita() +
+  theme(aspect.ratio = NULL) +
+  facet_wrap(~article_fc_rating, 
+             ncol = 1,
+             strip.position = "right",
+             labeller = labeller(article_fc_rating = label_veracity))
+gg_exposuretime
+
+# Percentiage of tweets per story
+gg_relexpostime <- ggplot(exposure_timeseries, aes(x = time, y = relative_cumulative_exposed, group = total_article_number)) +
+  geom_line(size = 0.3, alpha = 0.5) +
+  # scale_y_log10() +
+  scale_y_continuous(labels = scales::comma) +
+  xlab("Time since first article share (hrs)") +
+  ylab("Proportion of total users exposed") +
+  theme_ctokita() +
+  theme(aspect.ratio = NULL) +
+  facet_wrap(~article_fc_rating, 
+             ncol = 1,
+             strip.position = "right",
+             labeller = labeller(article_fc_rating = label_veracity))
+gg_relexpostime
+
+
+
