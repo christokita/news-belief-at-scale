a7ad8b933c9b55ccf7902bba49b962d0a5a247c0
Chris Tokita <christopher.tokita@gmail.com>
2020-07-24 20:15:25 -0400

    Create add_tweet_metadata.cmd

---
+++ slurm_scripts/add_tweet_metadata.cmd
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4
+#SBATCH --time=6:00:00
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --mail-user=ctokita@princeton.edu
+#SBATCH --output=slurm_outfiles/slurm-%A.out
+
+##Load anaconda python packages
+module load anaconda3 
+##Run script
+srun python3 scripts/03_add_tweet_metadata.py
