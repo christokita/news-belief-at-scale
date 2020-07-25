584ac32272778f4e2686cf45a6e9a43cf68d55d8
Chris Tokita <christopher.tokita@gmail.com>
2020-07-24 20:15:03 -0400

    calculate missing ideological scores

--- scripts/03_add_tweet_metadata.py
+++ scripts/03_add_tweet_metadata.py
@@ -16,19 +16,17 @@ import pandas as pd
 import numpy as np
 import re
 import os
-import multiprocessing as mp
-import datetime as dt
     
 # high level directory (external HD or cluster storage)
-#data_directory = "/scratch/gpfs/ctokita/fake-news-diffusion/" #HPC cluster storage
-data_directory = "/Volumes/CKT-DATA/fake-news-diffusion/" #external HD
+data_directory = "/scratch/gpfs/ctokita/fake-news-diffusion/" #HPC cluster storage
+#data_directory = "/Volumes/CKT-DATA/fake-news-diffusion/" #external HD
 
 
 ####################
 # Add article-level information
 ####################
 # Load tweets
-tweets = pd.read_csv(data_directory + "data_derived/tweets/parsed_tweets.csv")
+tweets = pd.read_csv(data_directory + "data_derived/tweets/tweets_parsed.csv")
 
 # Get source lean ratings
 articles = pd.read_csv(data_directory + "data/articles/daily_articles.csv")
@@ -87,9 +85,48 @@ ideological_scores.loc[ideological_scores['user_ideology'].between(-ideo_thresho
 # Merge in ideological scores
 labeled_tweeters = labeled_tweeters.merge(ideological_scores, how = 'left', on = 'user_id')
 
+####################
+# Calculate missing ideologies for tweeters 
+####################
+# These were users who SMAPP did not already have calculated
+# We will calculate their ideology using a simple mean of their friends
+
+# Determine missing ideologies
+missing_ideologies = labeled_tweeters[['user_id', 'user_ideology']].drop_duplicates()
+missing_ideologies = missing_ideologies[pd.isna(missing_ideologies.user_ideology)]
+
+# Load friend ideology scores
+friend_ideologies = pd.read_csv(data_directory + "data_derived/ideological_scores/cleaned_friends_ideology_scores.csv")
+
+# For each tweeter, calculate mean ideology of friends
+friend_files = os.listdir(data_directory + "data/friends/")
+#for i in np.arange(missing_ideologies.shape[0]):
+for i in np.arange(300):
+
+    # Get user ID and load followers
+    user = missing_ideologies['user_id'].iloc[i]
+    regex = re.compile(r"[0-9].*_%s.csv" % user)
+    file = list(filter(regex.match, friend_files))
+    
+    # Parse friends and calculate mean. If no followers, next person
+    if len(file) == 0: #no followers, no follower file
+            next
+    else:
+        try:
+            follower_list = np.genfromtxt(data_directory + "data/followers/" + file[0], dtype = int)
+            follower_list = follower_list[1:len(follower_list)] #remove header, will raise error if empty
+            friend_scores = friend_ideologies[friend_ideologies['user_id'].isin(follower_list)]
+            avg_score = np.mean(friend_scores['pablo_score'])
+            missing_ideologies['user_ideology'].iloc[i] = avg_score
+        except:
+            next
+    
+# Merge in new scores
+labeled_tweeters = labeled_tweeters.set_index('user_id').user_ideology.fillna(missing_ideologies.set_index('user_id').user_ideology).reset_index()
+labeled_tweeters['manual_ideology_score'] = labeled_tweeters['user_id'].isin(missing_ideologies['user_id'])
 
 ####################
 # Write to file
 ####################
 # Save
-labeled_tweeters.to_csv(data_directory + "data_derived/tweets/all_tweets_labeled.csv", index = False)
+labeled_tweeters.to_csv(data_directory + "data_derived/tweets/tweets_labeled.csv", index = False)
