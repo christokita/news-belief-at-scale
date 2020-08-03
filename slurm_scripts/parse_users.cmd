8a592714d5554eb4c6930754f882b2ef199aed1d
Chris Tokita <christopher.tokita@gmail.com>
2020-08-03 19:21:01 -0400

    Update parse_users.cmd

--- slurm_scripts/parse_users.cmd
+++ slurm_scripts/parse_users.cmd
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
-#SBATCH --mem-per-cpu=12gb
+#SBATCH --mem-per-cpu=18gb
 #SBATCH --array=0-299
 #SBATCH --time=11:59:00
 #SBATCH --mail-type=FAIL
