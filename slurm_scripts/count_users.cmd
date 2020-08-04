1e84481e0b881aa7840ea929447fae60238c0668
Chris Tokita <christopher.tokita@gmail.com>
2020-08-04 14:06:55 -0400

    Update count_users.cmd

--- slurm_scripts/count_users.cmd
+++ slurm_scripts/count_users.cmd
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=20
-#SBATCH --time=11:59:00
+#SBATCH --time=23:59:00
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --mail-user=ctokita@princeton.edu
 #SBATCH --output=slurm_outfiles/slurm-%A.out
