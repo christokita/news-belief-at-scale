81b60ef8ce0f3433ae75f5d0557b28640fb99a95
Chris Tokita <christopher.tokita@gmail.com>
2020-09-11 10:03:02 -0400

    Update compute_population_ideology_dist.cmd

--- slurm_scripts/compute_population_ideology_dist.cmd
+++ slurm_scripts/compute_population_ideology_dist.cmd
@@ -1,13 +1,14 @@
 #!/bin/bash
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=20
+#SBATCH --cpus-per-task=8
 #SBATCH --mem-per-cpu=6gb
-#SBATCH --time=23:59:00
+#SBATCH --time=140:00:00
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --mail-user=ctokita@princeton.edu
 #SBATCH --output=slurm_outfiles/slurm-%A.out
 
 ##Load anaconda python packages
+module purge
 module load anaconda3 
 conda activate my_conda
 ##Run script
