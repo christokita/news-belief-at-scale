839a8f15b1370e4bda098a1df11edea34defff8c
Chris Tokita <christopher.tokita@gmail.com>
2020-09-16 11:15:19 -0400

    Update handcheck_moderate_ideology.py

--- scripts/suppl_code/handcheck_moderate_ideology.py
+++ scripts/suppl_code/handcheck_moderate_ideology.py
@@ -19,8 +19,29 @@ labeled_tweets = pd.read_csv(data_directory + "data_derived/tweets/tweets_labele
                              dtype = {'quoted_urls': object, 'quoted_urls_expanded': object, #these two columns cause memory issues if not pre-specified dtype
                                       'user_id': object, 'tweet_id': 'int64'})
 
-# Output samples
+# Output samples - raw scores
 labeled_tweets['user_id_str'] = "\"" + labeled_tweets['user_id'] + "\""
 moderate_sample = labeled_tweets[['user_id', 'user_id_str', 'user_name', 'user_ideology', 'user_ideol_extremity']].drop_duplicates()
 moderate_sample = moderate_sample.nsmallest(200, 'user_ideol_extremity')
 moderate_sample.to_csv(data_directory + "data_derived/ideological_scores/sample_moderate_users.csv", index = False)
+
+# Output samples of normalized scores
+# Normalize
+upper_bound = 3.5
+lower_bound= -3.5
+max_val = max(labeled_tweets.user_ideology)
+min_val = min(labeled_tweets.user_ideology)
+labeled_tweets['normalized_ideology'] = (labeled_tweets['user_ideology'] - min_val) / (max_val - min_val)
+labeled_tweets['normalized_ideology'] = ( (upper_bound - lower_bound) *  labeled_tweets['normalized_ideology'] ) + lower_bound
+
+# Select examples
+labeled_tweets['normalized_ideol_extremity'] = abs(labeled_tweets['normalized_ideology'])
+norm_moderate_sample = labeled_tweets[['user_id', 'user_id_str', 'user_name', 'user_ideology', 'normalized_ideology', 'normalized_ideol_extremity']].drop_duplicates()
+norm_moderate_sample = norm_moderate_sample.nsmallest(200, 'normalized_ideol_extremity')
+norm_moderate_sample.to_csv(data_directory + "data_derived/ideological_scores/sample_moderate_users_normalized.csv", index = False)
+
+
+
+import matplotlib.pyplot as plt
+plt.scatter(labeled_tweets['user_ideology'], labeled_tweets['normalized_ideology'])
+plt.hist(labeled_tweets['normalized_ideology'], bins = np.arange(-3.5, 4, 1))
