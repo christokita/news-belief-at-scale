c5db9d4b0ec9cc30ce05e04efcc3d3da99b73e43
Chris Tokita <christopher.tokita@gmail.com>
2020-10-12 16:11:36 -0400

    ideologies of tweeters

--- scripts/plot_ideology.R
+++ scripts/plot_ideology.R
@@ -17,14 +17,18 @@ source("scripts/_plot_themes/theme_ctokita.R")
 ####################
 # Paramters for analysis: paths to data, paths for output, and filename
 ####################
-# Paths to data
-tweeter_score_path <- '/Volumes/CKT-DATA/fake-news-diffusion/data_derived/tweets/tweets_labeled.csv' 
-outpath <- 'output/ideology/'
-
 # Choose grouping of interest. Options: 
 #     (1) article veracity: "article_fc_rating"
 #     (2) source: "source_type"
-grouping <- "article_fc_rating"
+grouping <- "source_type"
+
+# Paths to data
+tweeter_score_path <- '/Volumes/CKT-DATA/fake-news-diffusion/data_derived/tweets/tweets_labeled.csv' 
+if (grouping == "article_fc_rating") {
+  outpath <- 'output/ideology/veracity/'
+} else if(grouping == "source_type") {
+  outpath <- 'output/ideology/source_type/'
+}
 
 # Color palette
 plot_color <- "#495867"
@@ -47,7 +51,16 @@ tweets <- read.csv(tweeter_score_path, header = TRUE, colClasses = c('user_id'='
          source_type = ifelse(source_type == "mainstream", "Mainstream", ifelse(source_type == "fringe", "Fringe", source_type)),
          article_lean = ifelse(article_lean == "C", "Conservative", ifelse(article_lean == "L", "Liberal",
                                                                            ifelse(article_lean == "N", "Neutral", 
-                                                                                  ifelse(article_lean == "U", "Unclear", source_type)))) )
+                                                                                  ifelse(article_lean == "U", "Unclear", source_type)))) ) %>% 
+  # bin ideologies
+  mutate(bin = cut(user_ideology, breaks = seq(-5, 5, 0.5))) %>% 
+  mutate(lower_edge = as.numeric( gsub("^[^0-9]([-\\.0-9]+),.*", "\\1", bin, perl = TRUE) ),
+         upper_edge = as.numeric( gsub(".*,([-\\.0-9]+)[^0-9]$", "\\1", bin, perl = TRUE) )) %>% 
+  mutate(ideology_bin = (lower_edge + upper_edge) / 2) %>% 
+  select(-bin, -lower_edge, -upper_edge) %>% 
+  # count number of articles per grouping (useful for average distributions) %>% 
+  group_by(!!sym(grouping)) %>% 
+  mutate(n_articles_in_grouping = length(unique(total_article_number)))
 
 # If analyzing by veracity, drop out non-True/False articles
 if (grouping == "article_fc_rating") {
@@ -65,37 +78,34 @@ shapes <- read.csv("/Volumes/CKT-DATA/fake-news-diffusion/data_derived/ideologic
 length(unique(tweets$user_id)) == length(unique(shapes$user_id))
 
 
-############################## Summary of ideology by article veracity ##############################
+
+############################## Summary of tweeter ideology ##############################
 
 ####################
-# Plot: Ideology by article veracity
+# Plot: Total tweets by ideology
 ####################
-# Summarise data
-tweeter_veracity <- tweets %>% 
-  group_by(article_fc_rating) %>% 
-  summarise(ideological_extremity = mean(user_ideol_extremity, na.rm = TRUE)) %>% 
-  filter(article_fc_rating %in% c("FM", "T"))
-
-# Plot ideological distribution
-gg_veracityideo <- tweets %>% 
-  filter(!is.na(user_ideology),
-         article_fc_rating %in% c("FM", "T")) %>% 
-  ggplot(., aes(x = user_ideology, fill = ..x..)) +
-  geom_histogram(position = 'identity', binwidth = 0.25) +
+gg_ideoltweets <- tweets %>% 
+  # Calculate tweets per ideology bin
+  filter(!is.na(user_ideology)) %>% 
+  group_by(!!sym(grouping), ideology_bin) %>% 
+  summarise(count = n()) %>% 
+  # plot
+  ggplot(., aes(x = ideology_bin, y = count, fill = ideology_bin)) +
+  geom_bar(stat = "identity") +
   xlab("Tweeter ideology") +
   ylab("Number of tweets") +
-  scale_x_continuous(limits = c(-4.5, 4.5), expand = c(0, 0), breaks = seq(-5, 5, 1)) +
+  scale_x_continuous(limits = c(-4.5, 5), expand = c(0, 0), breaks = seq(-5, 5, 1)) +
   scale_fill_gradientn(colors = ideol_pal, limits = c(-2, 2), oob = scales::squish) +
+  scale_color_gradientn(colors = ideol_pal, limits = c(-2, 2), oob = scales::squish) +
   theme_ctokita() +
-  theme(aspect.ratio = 0.2, 
+  theme(aspect.ratio = NULL, 
         legend.position = "none") +
-  facet_wrap(~article_fc_rating, 
+  facet_wrap(as.formula(paste("~", grouping)), 
              scales = 'free', 
              ncol = 1, 
-             strip.position = "right",
-             labeller = labeller(article_fc_rating = label_veracity))
-gg_veracityideo
-ggsave(gg_veracityideo, file = paste0(outpath, "ideologies_byveracity.png"), width = 90, height = 70, units = "mm", dpi = 400)
+             strip.position = "right")
+gg_ideoltweets
+ggsave(gg_ideoltweets, file = paste0(outpath, "total_tweets_by_ideology.png"), width = 90, height = 90, units = "mm", dpi = 400)
 
 
 # Plot ideological extremity
@@ -104,13 +114,101 @@ gg_veracityextr <- tweets %>%
   ggplot(., aes(x = user_ideol_extremity, fill = ..x..)) +
   geom_histogram(position = 'identity', binwidth = 0.25) +
   theme_ctokita() +
-  facet_wrap(~article_fc_rating, 
+  facet_wrap(as.formula(paste("~", grouping)), 
              scales = 'free', 
              ncol = 1, 
              strip.position = "right")
 gg_veracityextr
 
 
+####################
+# Plot: Average ideology distribution of tweeters
+####################
+gg_ideoldist_tweeters <- tweets %>% 
+  # Calculate distribution of tweeter ideologies per article
+  filter(!is.na(user_ideology)) %>% 
+  group_by(n_articles_in_grouping, !!sym(grouping), total_article_number, ideology_bin) %>% 
+  summarise(count = n()) %>% 
+  ungroup() %>% 
+  group_by(!!sym(grouping), total_article_number) %>% 
+  mutate(tweeter_prop = count / sum(count)) %>% 
+  # Now determine average distribution shape by article grouping
+  group_by(!!sym(grouping), ideology_bin) %>% 
+  summarise(avg_tweeter_prop = sum(tweeter_prop) / unique(n_articles_in_grouping)) %>% 
+  # Plot
+  ggplot(., aes(x = ideology_bin, y = avg_tweeter_prop, fill = ideology_bin)) +
+  geom_bar(stat = "identity") +
+  xlab("Tweeter ideology") +
+  ylab("Avg. proportion of article tweeters") +
+  scale_x_continuous(limits = c(-4.5, 5), expand = c(0, 0), breaks = seq(-5, 5, 1)) +
+  scale_fill_gradientn(colors = ideol_pal, limits = c(-2, 2), oob = scales::squish) +
+  theme_ctokita() +
+  theme(aspect.ratio = NULL, 
+        legend.position = "none") +
+  facet_wrap(as.formula(paste("~", grouping)), 
+             scales = 'free', 
+             ncol = 1, 
+             strip.position = "right")
+gg_ideoldist_tweeters
+ggsave(gg_ideoldist_tweeters, file = paste0(outpath, "average_tweeter_ideoldist.png"), width = 90, height = 90, units = "mm", dpi = 400)
+
+
+####################
+# Plot: Average ideology distribution of retweets only
+####################
+gg_ideoldist_retweet <- tweets %>% 
+  # Calculate distribution of tweeter ideologies per article
+  filter(!is.na(user_ideology),
+         is_retweet == "True") %>% 
+  group_by(n_articles_in_grouping, !!sym(grouping), total_article_number, ideology_bin) %>% 
+  summarise(count = n()) %>% 
+  ungroup() %>% 
+  group_by(!!sym(grouping), total_article_number) %>% 
+  mutate(tweeter_prop = count / sum(count)) %>% 
+  # Now determine average distribution shape by article grouping
+  group_by(!!sym(grouping), ideology_bin) %>% 
+  summarise(avg_tweeter_prop = sum(tweeter_prop) / unique(n_articles_in_grouping)) %>% 
+  # Plot
+  ggplot(., aes(x = ideology_bin, y = avg_tweeter_prop, fill = ideology_bin)) +
+  geom_bar(stat = "identity") +
+  xlab("Tweeter ideology") +
+  ylab("Avg. proportion of article re-tweeters") +
+  scale_x_continuous(limits = c(-4.5, 5), expand = c(0, 0), breaks = seq(-5, 5, 1)) +
+  scale_fill_gradientn(colors = ideol_pal, limits = c(-2, 2), oob = scales::squish) +
+  theme_ctokita() +
+  theme(aspect.ratio = NULL, 
+        legend.position = "none") +
+  facet_wrap(as.formula(paste("~", grouping)), 
+             scales = 'free', 
+             ncol = 1, 
+             strip.position = "right")
+gg_ideoldist_retweet
+ggsave(gg_ideoldist_retweet, file = paste0(outpath, "average_retweeter_ideoldist.png"), width = 90, height = 90, units = "mm", dpi = 400)
+
+# Plot by total article number
+gg_ideoldist_retweet <- tweets %>% 
+  # Calculate distribution of tweeter ideologies per article
+  filter(!is.na(user_ideology),
+         # is_retweet == "True",
+         article_fc_rating == "Fake news") %>% 
+  group_by(!!sym(grouping), total_article_number, ideology_bin) %>% 
+  summarise(count = n()) %>% 
+  ungroup() %>% 
+  group_by(!!sym(grouping), total_article_number) %>% 
+  mutate(tweeter_prop = count / sum(count)) %>% 
+  # Plot
+  ggplot(., aes(x = ideology_bin, y = tweeter_prop, fill = ideology_bin)) +
+  geom_bar(stat = "identity") +
+  xlab("Tweeter ideology") +
+  ylab("Avg. proportion of article re-tweeters") +
+  scale_x_continuous(limits = c(-4.5, 5), expand = c(0, 0), breaks = seq(-5, 5, 1)) +
+  scale_fill_gradientn(colors = ideol_pal, limits = c(-2, 2), oob = scales::squish) +
+  theme_ctokita() +
+  theme(aspect.ratio = NULL, 
+        legend.position = "none") +
+  facet_wrap(~total_article_number, 
+             scales = 'free')
+gg_ideoldist_retweet
 
 ############################## Plot follower distribution shapes ##############################
 
