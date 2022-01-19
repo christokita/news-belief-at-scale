56ce7049508caaee685a5856da8a46afc072bd7c
Chris Tokita <christopher.tokita@gmail.com>
2022-01-18 18:20:02 -0800

    add friend/followers of missing tweeters

--- scripts/suppl_code/getting_more_data/prep_new_data.py
+++ scripts/suppl_code/getting_more_data/prep_new_data.py
@@ -88,3 +88,31 @@ for user in unique_accounts_for_friends:
     
         
 ########## Missing Tweeters ########## 
+####################
+# Break up combined follower lists into indiviudal follower lists like the rest of the data
+####################
+# Read in follower list(s) for new tweeters
+missing_tweeter_followers = pd.read_csv(data_directory + 'data/followers/_new_tweeters_followers_jan2022/followers_tweeters_missing_articles.csv', dtype = object)
+
+# Loop through each unique tweeter, get their individual list of followers, and write to file
+unique_accounts_for_followers = np.unique(missing_tweeter_followers['account_id'])
+for user in unique_accounts_for_followers:
+    followers = missing_tweeter_followers[missing_tweeter_followers.account_id == user]
+    followers = followers[['follower_id']]
+    followers = followers.rename(columns = {'follower_id': 'user_id_followers'}) #this is how our original data is formatted
+    followers.to_csv(data_directory + 'data/followers/2022__01__18__' + user + '.csv', index = False) #standard file format 
+    
+    
+####################
+# Break up combined friend lists into indiviudal friend lists like the rest of the data
+####################
+# Read in friend list(s) for new tweeters
+missing_tweeter_friends = pd.read_csv(data_directory + 'data/friends/_new_tweeters_friends_jan2022/friends_tweeters_missing_articles.csv', dtype = object)
+
+# Loop through each unique tweeter, get their individual list of friends, and write to file
+unique_accounts_for_friends = np.unique(missing_tweeter_friends['account_id'])
+for user in unique_accounts_for_friends:
+    friends = missing_tweeter_friends[missing_tweeter_friends.account_id == user]
+    friends = friends[['friend_id']]
+    friends = friends.rename(columns = {'friend_id': 'user_id_friends'}) #this is how our original data is formatted
+    friends.to_csv(data_directory + 'data/friends/2022__01__18__' + user + '.csv', index = False) #standard file format 
