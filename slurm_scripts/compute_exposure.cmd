37c52b2918f9a2c950fd335dcdd8e041f667337e
Chris Tokita <christopher.tokita@gmail.com>
2022-03-12 13:47:06 -0800

    udpate compute_exposure.cmd
    We now have 149 unique stories represented in our tweets, so we needed to increase the sbatch array to reflect the real number of jobs we need.

--- slurm_scripts/compute_exposure.cmd
+++ slurm_scripts/compute_exposure.cmd
@@ -1,14 +1,14 @@
 #!/bin/bash
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=2
-#SBATCH --mem-per-cpu=24gb
-#SBATCH --array=0-135
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=80gb
+#SBATCH --array=0-148
 #SBATCH --time=23:59:00
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --mail-user=ctokita@princeton.edu
 #SBATCH --output=slurm_outfiles/slurm-%A_%a.out
 
-## NOTE: We have 136 unique stories as of now, hence the job array of 0 to 135 (zero indexing in python).
+## NOTE: We have 149 unique stories as of now, hence the job array of 0 to 148 (zero indexing in python).
 
 ##Load anaconda python packages
 module load anaconda3/2021.11 
