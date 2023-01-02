1b7ce28e6f849cd7a9c974f602b4a0ce11a80633
Chris Tokita <christopher.tokita@gmail.com>
2023-01-02 16:12:52 -0600

    Update plot_exposure.R

--- scripts/plot_exposure.R
+++ scripts/plot_exposure.R
@@ -42,7 +42,7 @@ DATA_DIRECTORY <- "/Volumes/CKT-DATA/news-belief-at-scale/"
 # Choose GROUPING of interest. Options: 
 #     (1) article veracity: "article_fc_rating"
 #     (2) source: "source_type"
-GROUPING <- "source_type"
+GROUPING <- "article_fc_rating"
 
 
 ####################
